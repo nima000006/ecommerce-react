278f630ffb87d7154355881482b5a437cce2f980
nima <nimachehreh690@gmail.com>
2022-03-26 00:58:52 +0430

    first component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class NavBar extends Component {
+  render() {
+    return (
+        <React.Fragment>
+          <h1>Hello</h1>
+          <p>h2</p>
+        </React.Fragment>
+        
+    )
+  }
+}
+export default NavBar;
